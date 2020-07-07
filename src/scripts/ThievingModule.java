764401ef8101431b3ee6559612f436b9be98d92a
Lassi Haapala <lassi.p.haapala@utu.fi>
2020-07-07 17:52:29 +0300

    Thieving module base

--- src/scripts/ThievingModule.java
+++ src/scripts/ThievingModule.java
@@ -1,44 +1,119 @@
 package scripts;
+
 import org.dreambot.api.methods.skills.Skill;
 
-public class ThievingModule extends ScriptModule {
+import antiban.RandomProvider;
+import client.ClientThread;
+import client.ThreadController;
+import movement.LocationFactory;
+import utilities.GearHandler.Gear;
 
-	//TODO:
+public class ModuleTemplate extends ScriptModule {
+	//CLASS OBJECTS HERE:
+	
+	private ThreadController controller;
+	private ClientThread client;
+
+	private boolean killThread;
+	
+	private int limit;
+	private int actionsCompleted;
+
+	private String targetName;
+	
+	private LocationFactory.GameLocation locationEnum;
+	private ThievingTarget targetEnum;
+	
+	//MODULE CONSTRUCTOR:
+	public ModuleTemplate(ThreadController controller, ClientThread client, LocationFactory.GameLocation locationEnum, 
+						 ThievingTarget target, int limit) { //<-- ADD REQUIRED OBJECTS HERE
+		this.controller = controller; 
+		this.client = client;
+		this.limit = limit;
+		this.locationEnum = locationEnum;
+		this.targetEnum = target;
+		this.setTargetVariables(target);
+	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		while(!killThread) {
+			RandomProvider.sleep(1000,1500);
+
+			if(!client.getLocalPlayer().isAnimating()){
+				controller.getGraphicHandler().setInfo("ThievingModule: Thieving");
+				NPC npc = client.getNPCs().closest( f -> f != null && f.getName().equals(this.targetName));
+				if(RandomProvider.fiftyfifty()){
+					npc.interact();
+					client.getMouse().move();
+				}
+				else{
+					npc.interact("Pickpocket");
+					client.getMouse().move();
+				}
+
+			}
+
+		}
+		
 	}
 	
 	@Override
-	public boolean setupModule() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean setupModule() {//Return true if setup completed, otherwise false IE. bank dosent have required items
+		
+		this.controller.getMovementHandler().newLocation(this.locationEnum);
+		
+		this.controller.getMovementHandler().teleportToLocation();
+		
+		this.controller.getGearHandler().handleGearSwap(Gear.UTILITY); //TODO: SWAP TO CORRECT GEAR
+		
+		controller.getGraphicHandler().setInfo("ThievingModule: Setting up module"); //TODO: CUSTOMIZE MESSAGE
+		
+		return true;
 	}
-
+	
 	@Override
 	public Skill getSkillToHover() {
-		// TODO Auto-generated method stub
-		return null;
+		return Skill.THIEVING; //TODO: SWAP TO CORRECT SKILL
 	}
-
-
+	
 	@Override
 	public void killThread() {
-		// TODO Auto-generated method stub
-		
+		this.killThread = true;
 	}
 
 	@Override
 	public boolean isAlive() {
-		// TODO Auto-generated method stub
-		return false;
+		return !killThread;
 	}
 
 	@Override
 	public boolean isReady() {
-		// TODO Auto-generated method stub
-		return false;
+		if(this.limit <= this.actionsCompleted) {
+			return true;
+		}
+		else { 
+			return  false; 
+		}
+	
 	}
+
+	private void setTargetVariables(ThievingTarget target){
+		if(target == ThievingTarget.MAN){
+
+		}
+		else if(target == ThievingTarget.FARMER){
+
+		}
+		else if(target == ThievingTarget.MASTER_FARMER){
+
+		}
+	}
+
+	public enum ThievingTarget{
+		MAN, FARMER, MASTER_FARMER;
+	}
+
 
 }
